21f7a09a4326b5bc33d006002ec6ec8de96d70fd
Nenad <nbursa@gmail.com>
2024-07-13 22:19:40 +0200

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,13 @@ use std::process;
 
 use clap::{Arg, Command};
 use pulldown_cmark::{html::push_html, Parser};
+use log::{error, info};
+use env_logger;
 
 fn main() {
+    // Initialize the logger
+    env_logger::init();
+
     let matches = Command::new("Markdown to HTML Converter")
         .version("1.0")
         .author("Nenad <nbursa@gmail.com>")
@@ -24,9 +29,11 @@ fn main() {
 
     // Read the markdown file
     let markdown_input = fs::read_to_string(input_file).unwrap_or_else(|err| {
-        eprintln!("Error reading input file {}: {}", input_file, err);
+        error!("Error reading input file {}: {}", input_file, err);
         process::exit(1);
     });
+
+    info!("Successfully read input file: {}", input_file);
 
     let parser = Parser::new(&markdown_input);
 
@@ -34,9 +41,11 @@ fn main() {
     push_html(&mut html_output, parser);
 
     fs::write(output_file, html_output).unwrap_or_else(|err| {
-        eprintln!("Error writing output file {}: {}", output_file, err);
+        error!("Error writing output file {}: {}", output_file, err);
         process::exit(1);
     });
+
+    info!("Successfully wrote to output file: {}", output_file);
 
     println!("Successfully converted {} to {}", input_file, output_file);
 }
